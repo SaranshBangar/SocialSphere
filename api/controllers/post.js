a39fa32ff023b7a00def8ac4786ee02e6f199240
Saransh Bangar <saranshbangad@gmail.com>
2024-05-01 15:58:02 +0530

    Added comments and auth

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,13 +1,22 @@
 import { db } from "../connect.js"
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
-    const q = `SELECT p.*, u.id AS userid, name, proficPic FROM posts AS p JOIN users AS u ON (u.id = p.userid)`;
 
-    db.query(q, (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Internal server error" });
-        }
-        return res.status(200).json(data);
-    });
+    const token = req.cookies.accessToken;
+    if (!token)
+        return res.status(401).json("Unauthorized");
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err)
+            return res.status(403).json("Token invalid");
+
+        const q = `SELECT p.*, u.id AS userid, name, proficPic FROM posts AS p JOIN users AS u ON (u.id = p.userid) LEFT JOIN relationships AS r ON (r.followeduserid = p.userid) WHERE r.followeruserid = ? OR p.userid = ?`;
+
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+            if (err)
+                return res.status(500).json(err);
+            return res.status(200).json(data);
+        });
+    })
 }
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
   Outlet,
   Navigate,
 } from "react-router-dom";
@@ -16,14 +15,14 @@ import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   const { darkMode } = useContext(DarkModeContext);
 
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   const Layout = () => {
     return (
